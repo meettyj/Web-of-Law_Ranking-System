19f1a44d38c233d798732265a15ed75aa854b82f
Yijun Tian <yt1506@grice.cs.nyu.edu>
2019-01-27 11:49:00 -0500

    Initial commit

---
+++ generate_corpus.py
@@ -0,0 +1,26 @@
+import os
+
+def main():
+	# out_dir = './data/test_scotus_corpus.txt'
+	out_dir = './data/all_scotus_corpus.txt'
+	corpus_dir = '../all_scotus_text/'
+	# corpus_dir = './data/1K_scotus_text/'
+	# corpus_dir = './data/test_scotus_text/'
+	corpus_files = os.listdir(corpus_dir)
+	# id_corpus = 0
+	with open(out_dir, 'a') as out_f:
+		for file in corpus_files:
+			# id_corpus += 1
+			file_name = file.split('.')[0]
+			# print(file.split('.')[0])
+			# out_f.write('# '+ str(id_corpus) + '\n')
+			out_f.write('# '+ file_name + '\n')
+			# out_f.write(str(id_corpus))
+			with open(corpus_dir + file, mode='r') as each_f:
+				all_text = each_f.read()
+				out_f.write(all_text)
+	print('finished generated')
+
+
+if __name__ == '__main__':
+	main()
