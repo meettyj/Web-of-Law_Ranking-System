276047d503d3ac4e0a73a549c51421ca76fd17dc
meettyj <meettianyijun@gmail.com>
2019-03-30 13:26:05 -0400

    remove annotation tool

--- ttttt/app/server/static/js/dataset.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-
-const vm = new Vue({
-  el: '#mail-app',
-  delimiters: ['[[', ']]'],
-  data: {
-    messages: []
-  },
-});
--- ttttt/tools/ci.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit
-
-flake8
-python app/manage.py migrate
-python app/manage.py collectstatic
-python app/manage.py test server.tests
